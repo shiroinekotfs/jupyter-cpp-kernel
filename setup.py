59647c332d1931c02f52804cb968e5760e2df595
滝猫 <systakineko.tfs@gmail.com>
2023-07-13 12:50:34 +0700

    Update req pkgs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='jupyter_cpp_kernel',
       version='1.0.0',
-      description='Minimalistic C++ kernel for Jupyter',
+      description='C++ kernel for Jupyter',
       author='Tsuki Takineko (ft. Brendan Rius)',
       author_email='systakineko.tfs@gmail.com',
       license='MIT',
@@ -13,6 +13,6 @@ setup(name='jupyter_cpp_kernel',
       download_url='',
       packages=['jupyter_cpp_kernel'],
       scripts=['jupyter_cpp_kernel/install_cpp_kernel'],
-      keywords=['jupyter', 'notebook', 'kernel', 'cpp'],
+      keywords=['jupyter', 'cpp14', 'jupyter_kernels', 'cpp', 'pip'],
       include_package_data=True
       )
