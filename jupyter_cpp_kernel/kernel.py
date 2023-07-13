5f7c93362a8e53ba454f06ef4775ebb5da270723
滝猫 <systakineko.tfs@gmail.com>
2023-07-13 18:07:03 +0700

    Allow user inputs into kernel
    Co-Authored-By: gradyfitz <12127273+gradyfitz@users.noreply.github.com>

--- jupyter_cpp_kernel/kernel.py
+++ jupyter_cpp_kernel/kernel.py
@@ -1,10 +1,12 @@
-import re, subprocess, tempfile, os
-
-import os.path as path
-from os import system as cs
 from queue import Queue
 from threading import Thread
+from os import system as cs
 from ipykernel.kernelbase import Kernel
+import re
+import subprocess
+import tempfile
+import os
+import os.path as path
 
 class RealTimeSubprocess(subprocess.Popen):
     def __init__(self, cmd, write_to_stdout, write_to_stderr):
@@ -16,7 +18,7 @@ class RealTimeSubprocess(subprocess.Popen):
         self._write_to_stdout = write_to_stdout
         self._write_to_stderr = write_to_stderr
 
-        super().__init__(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=0)
+        super().__init__(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=0)
 
         self._stdout_queue = Queue()
         self._stdout_thread = Thread(target=RealTimeSubprocess._enqueue_output, args=(self.stdout, self._stdout_queue))
@@ -30,14 +32,13 @@ class RealTimeSubprocess(subprocess.Popen):
 
     @staticmethod
     def _enqueue_output(stream, queue):
-        """
-        Add chunks of data from a stream to a queue until the stream is empty.
-        """
+        # Add chunks of data from a stream to a queue until the stream is empty.
         for line in iter(lambda: stream.read(4096), b''):
             queue.put(line)
         stream.close()
 
     def write_contents(self):
+        # Write the available content from stdin and stderr where specified when the instance was created
         def read_all_from_queue(queue):
             res = b''
             size = queue.qsize()
@@ -55,7 +56,6 @@ class RealTimeSubprocess(subprocess.Popen):
 
 
 class CPPKernel(Kernel):
-
     try:
         gcc_version = cs('gcc --version')
     except:
@@ -73,7 +73,7 @@ class CPPKernel(Kernel):
     language_info = {'name': 'cpp',
                      'mimetype': 'text/plain',
                      'file_extension': '.cpp'
-                    }
+                     }
     
     banner = "C++ 14 kernel for Jupyter\n\nGCC version:\n" + gcc_version + "\n\nG++ version:\n" + gpp_version + "\n\n"
 
@@ -87,14 +87,16 @@ class CPPKernel(Kernel):
         subprocess.call(['g++', filepath, '-std=c++14', '-rdynamic', '-ldl', '-o', self.master_path])
 
     def cleanup_files(self):
-        """Remove all the temporary files created by the kernel"""
+        # Remove all the temporary files created by the kernel
         for file in self.files:
             os.remove(file)
         os.remove(self.master_path)
 
     def new_temp_file(self, **kwargs):
-        """Create a new temp file to be deleted when the kernel shuts down"""
-        # We don't want the file to be deleted when closed, but only when the kernel stops
+        """
+        Create a new temp file to be deleted when the kernel shuts down
+        We don't want the file to be deleted when closed, but only when the kernel stops
+        """
         kwargs['delete'] = False
         kwargs['mode'] = 'w'
         file = tempfile.NamedTemporaryFile(**kwargs)
@@ -107,30 +109,64 @@ class CPPKernel(Kernel):
     def _write_to_stderr(self, contents):
         self.send_response(self.iopub_socket, 'stream', {'name': 'stderr', 'text': contents})
 
-    def create_jupyter_subprocess(self, cmd):
+    def create_jupyter_subprocess(self, cmd, input_text=""):
         return RealTimeSubprocess(cmd,
                                   lambda contents: self._write_to_stdout(contents.decode()),
                                   lambda contents: self._write_to_stderr(contents.decode()))
 
-    def compile_with_gcc(self, source_filename, binary_filename, cflags=None, ldflags=None):
-        cflags = ['-std=c++14', '-fPIC', '-shared', '-rdynamic'] + cflags
+    def compile_with_gpp(self, source_filename, binary_filename, cflags=None, ldflags=None):
         args = ['g++', source_filename] + cflags + ['-o', binary_filename] + ldflags
-        return self.create_jupyter_subprocess(args)
+        return self.create_jupyter_subprocess(cmd=args)
 
     def _filter_magics(self, code):
 
         magics = {'cflags': [],
                   'ldflags': [],
+                  'stdin': "",
+                  'stdout': "",
+                  'memtotalnoterm': "",
+                  'memtotal': "",
+                  'memaux': "",
+                  'memexpect': "",
+                  'test_script': [],
                   'args': []}
 
         for line in code.splitlines():
             if line.startswith('//%'):
-                key, value = line[3:].split(":", 2)
-                key = key.strip().lower()
+                try:
+                    key, value = line[3:].split(":", 2)
+                    key = key.strip().lower()
+                except ValueError:
+                    # Don't want kernel to fail because an error was made in source code comment.
+                    continue
 
                 if key in ['ldflags', 'cflags']:
                     for flag in value.split():
                         magics[key] += [flag]
+                elif key == "stdin":
+                    # Could probably be a match instead,
+                    # but this is easier for now.
+                    # Note: this is very basic, string escapes and the like
+                    #   aren't included.
+                    for stringContents in re.findall(r'\s*"([^"]*)"', value):
+                        magics['stdin'] += stringContents + "\n"
+                elif key == "stdout":
+                    # Could probably be a match instead,
+                    # but this is easier for now.
+                    # Note: this is very basic, string escapes and the like
+                    #   aren't included.
+                    for stringContents in re.findall(r'\s*"([^"]*)"', value):
+                        magics['stdout'] += stringContents + "\n"
+                elif key == "test_script":
+                    magics['test_script'].append(value)
+                elif key == "memtotalnoterm":
+                    magics['memtotalnoterm'] += "If you only allocated " + value + " bytes, you have likely left off space for string termination.\n"
+                elif key == "memtotal":
+                    magics['memtotal'] += "If you only allocated " + value + " bytes, you may not have allocated space for the array, which may or may not be what you wanted to do.\n"
+                elif key == "memaux":
+                    magics['memaux'] += "If you only allocated " + value + " bytes, you may not have allocated space for each string in the array, only the pointers to each string.\n"
+                elif key == "memexpect":
+                    magics['memexpect'] += "You should expect to use about " + value + " bytes, you can use valgrind and gdb to check that you've got this right in next week's workshop.\n"
                 elif key == "args":
                     # Split arguments respecting quotes
                     for argument in re.findall(r'(?:[^\s,"]|"(?:\\.|[^"])*")+', value):
@@ -147,26 +183,54 @@ class CPPKernel(Kernel):
             source_file.write(code)
             source_file.flush()
             with self.new_temp_file(suffix='.out') as binary_file:
-                p = self.compile_with_gcc(source_file.name, binary_file.name, magics['cflags'], magics['ldflags'])
+                p = self.compile_with_gpp(source_file.name, binary_file.name, magics['cflags'], magics['ldflags'])
                 while p.poll() is None:
                     p.write_contents()
                 p.write_contents()
                 if p.returncode != 0:  # Compilation failed
+                    self._write_to_stderr("Kernel location: " + os.path.dirname(os.path.realpath(__file__)) + "\n")
                     self._write_to_stderr(
-                            "[C++ 14 kernel] g++ exited with code {}, the code will not be executed".format(
+                            "[C++ 14 kernel] G++ exited with code {}, the code will not be executed".format(
                                     p.returncode))
                     return {'status': 'ok', 'execution_count': self.execution_count, 'payload': [],
                             'user_expressions': {}}
 
-        p = self.create_jupyter_subprocess([self.master_path, binary_file.name] + magics['args'])
+        p = self.create_jupyter_subprocess(cmd=([binary_file.name] + magics['args']))
+        if magics['stdin'] != "":
+            self._write_to_stderr(("input: " + magics['stdin']))
+        if magics['stdout'] != "":
+            self._write_to_stderr(("expected output: " + magics['stdout']))
+        if magics['memtotalnoterm'] != "" or magics['memtotal'] != "" or magics['memaux'] != "" or magics['memexpect'] != "":
+            self._write_to_stderr("Some memory hints which you might like to verify for this question:\n")
+        if magics['memtotalnoterm'] != "":
+            self._write_to_stderr(magics['memtotalnoterm'])
+        if magics['memtotal'] != "":
+            self._write_to_stderr(magics['memtotal'])
+        if magics['memaux'] != "":
+            self._write_to_stderr(magics['memaux'])
+        if magics['memexpect'] != "":
+            self._write_to_stderr(magics['memexpect'])
+        p.stdin.write(magics['stdin'].encode(encoding="utf-8", errors="strict"))
+        p.stdin.close()
         while p.poll() is None:
             p.write_contents()
         p.write_contents()
 
         if p.returncode != 0:
-            self._write_to_stderr("[C++ 14 kernel] Executable exited with code {}".format(p.returncode))
+            self._write_to_stderr("[C++ kernel] Executable exited with code {}".format(p.returncode))
+
+        if len(magics['test_script']) > 0:
+            for item in magics['test_script']:
+                self._write_to_stderr("Testing with script: " + item + ".\n")
+                subproc_commands = [['chmod'] + ['777'] + [item]] + [['/bin/bash'] + [item] + [binary_file.name]]
+                for subproc_command in subproc_commands:
+                    self._write_to_stderr("Command: " + str(subproc_command) + '\n')
+                    test_process = self.create_jupyter_subprocess(cmd=subproc_command)
+                    while test_process.poll() is None:
+                        test_process.write_contents()
+                    test_process.write_contents()
+
         return {'status': 'ok', 'execution_count': self.execution_count, 'payload': [], 'user_expressions': {}}
 
     def do_shutdown(self, restart):
-        """Cleanup the created source code files and executables when shutting down the kernel"""
         self.cleanup_files()
