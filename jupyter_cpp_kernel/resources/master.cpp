bc59fe45fc992cd00a1b58ab0364214696ff6699
滝猫 <systakineko.tfs@gmail.com>
2023-07-13 12:47:22 +0700

    Update master.cpp

--- jupyter_cpp_kernel/resources/master.cpp
+++ jupyter_cpp_kernel/resources/master.cpp
@@ -1,33 +1,29 @@
+#include <cstdlib>
+#include <cstdio>
 #include <iostream>
 #include <dlfcn.h>
 
-// Using namespace std to shorten the code. Don't ask me why
-using namespace std;
-
 typedef int (*main_t)(int, char **, char **);
 
-int main(int argc, char **argv, char **envp)
-{
-    char *error = NULL;
-
-    cout.setstate(ios_base::failbit);
-    cerr.setstate(ios_base::failbit);
+int main(int argc, char **argv, char **envp) {
+    char *error = nullptr;
+    setbuf(stdout, nullptr);
+    setbuf(stderr, nullptr);
     if (argc < 2) {
-        cerr << "USAGE: " << argv[0] << " PROGRAM\nWhere PROGRAM is the user's program to supervise\n";
+        fprintf(stderr, "USAGE: %s PROGRAM\nWhere PROGRAM is the user's program to supervise\n", argv[0]);
         return EXIT_FAILURE;
     }
     void *userhandle = dlopen(argv[1], RTLD_LAZY);
-    if (userhandle == NULL) {
-        cerr << argv[0] << ": " << dlerror() << endl;
+    if (userhandle == nullptr) {
+        fprintf(stderr, "%s: %s\n", argv[0], dlerror());
         return EXIT_FAILURE;
     }
     dlerror();
-    main_t usermain = dlsym(userhandle, "main");
-    if ((error = dlerror()) != NULL) {
-        cerr << argv[0] << ": " << error << endl;
+    main_t usermain = reinterpret_cast<main_t>(dlsym(userhandle, "main"));
+    if ((error = dlerror()) != nullptr) {
+        fprintf(stderr, "%s: %s\n", argv[0], error);
         return EXIT_FAILURE;
     }
-
-    /* Call Users main, but make master.cpp invisible by removing first argument */
+    /* Call Users main, but make master.c invisible by removing first argument */
     return usermain(argc-1, argv+1, envp);
 }
