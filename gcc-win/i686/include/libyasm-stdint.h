d82971a5c49e14b4963083169a5c6b468d072666
つき 滝猫 <systakineko.tfs@gmail.com>
2023-08-07 15:52:44 +0700

    Remove all weird gcc

--- gcc-win/i686/include/libyasm-stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _YASM_LIBYASM_STDINT_H
-#define _YASM_LIBYASM_STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "yasm 1.3.0"
-/* generated using /d/Prog/winlibs32ucrt_stage/mingw32/bin/i686-w64-mingw32-gcc.exe */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
--- gcc-win/x86-64/include/libyasm-stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _YASM_LIBYASM_STDINT_H
-#define _YASM_LIBYASM_STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "yasm 1.3.0"
-/* generated using /d/Prog/winlibs64ucrt_stage/mingw64/bin/x86_64-w64-mingw32-gcc.exe */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
