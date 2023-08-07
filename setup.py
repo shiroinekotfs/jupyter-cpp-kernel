2265fc4ab9b68192df66df42bda310365f13d32a
つき 滝猫 <systakineko.tfs@gmail.com>
2023-08-07 19:31:00 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 from setuptools import setup
-import os.path
-import glob
-
-here = os.path.dirname(__file__)
 
 setup(name='jupyter_cpp_kernel',
       version='1.0.0a3',
@@ -11,16 +7,20 @@ setup(name='jupyter_cpp_kernel',
       author_email='systakineko.tfs@gmail.com',
       license='MIT',
       classifiers=[
-          'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: C++',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
       ],
       url='https://github.com/takinekotfs/jupyter-cpp-kernel',
       download_url='https://github.com/takinekotfs/jupyter-cpp-kernel',
       packages=['jupyter_cpp_kernel'],
-      scripts=['jupyter_cpp_kernel/install_cpp_kernel'],
       keywords=['jupyter', 'cpp', 'cpp14', 'jupyter-kernels', 'pip'],
       include_package_data=True,
       data_files=[
           ("share/jupyter/kernels/cpp14",
-           glob.glob(os.path.join(here, "kernel_spec/*")))
+           ["kernel_spec/logo-64x64.png", "kernel_spec/kernel.json", "kernel_spec/logo-32x32.png", "kernel_spec/logo-svg.svg"]),
       ]
 )
