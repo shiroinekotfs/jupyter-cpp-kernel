58aa89e6a35b894e46818369dc10ff274f1a480a
つき 滝猫 <systakineko.tfs@gmail.com>
2023-08-07 18:38:13 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+import os.path
+import glob
+
+here = os.path.dirname(__file__)
 
 setup(name='jupyter_cpp_kernel',
       version='1.0.0a3',
@@ -15,4 +19,8 @@ setup(name='jupyter_cpp_kernel',
       scripts=['jupyter_cpp_kernel/install_cpp_kernel'],
       keywords=['jupyter', 'cpp', 'cpp14', 'jupyter-kernels', 'pip'],
       include_package_data=True
-      )
+      data_files=[
+          ("share/jupyter/kernels/cpp14",
+           glob.glob(os.path.join(here, "kernel_spec/*")))
+      ]
+)
