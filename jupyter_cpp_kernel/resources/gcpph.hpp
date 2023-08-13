44e0a18f7256fb27cc05bfc238ac43531e168cf7
つき 滝猫 <systakineko.tfs@gmail.com>
2023-08-13 19:13:32 +0700

    Update gcpph.hpp

--- jupyter_cpp_kernel/resources/gcpph.hpp
+++ jupyter_cpp_kernel/resources/gcpph.hpp
@@ -2,7 +2,6 @@
 #define _gcpph_hpp
 
 //global
-
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -16,14 +15,12 @@ extern "C" {
     #include <cstdlib>
     #include <cstdio>
     #include "basicf/rstdio.h"
-
     //dlfcn.h is not available on G++ for Windows
     #ifdef _WIN32
         #include "basicf/dlfcn.h" //local
     #else
         #include <dlfcn.h> //global
     #endif
-
 }
 
 #define GET_INPUT_STREAM_JP "<inputRequest>"
