7e49110d4dde691cdb178a30aafe4a13a91efdef
つき 滝猫 <systakineko.tfs@gmail.com>
2023-08-13 17:10:46 +0700

    Update test.cpp

--- jupyter_cpp_kernel/resources/test.cpp
+++ jupyter_cpp_kernel/resources/test.cpp
@@ -1,17 +1,7 @@
 //test.cpp is only can be used for testing
 //  - By Tsuki Takineko
 
-extern "C" {
-    #include <cstdlib>
-    #include <cstdio>
-    #include "basicf/rstdio.h"
-    //dlfcn.h is not available on G++ for Windows
-    #ifdef _WIN32
-        #include "basicf/dlfcn.h" //local
-    #else
-        #include <dlfcn.h> //global
-    #endif
-}
+#include "gcpph.hpp"
 
 int main() {
     std::string i;
