18e9233601e33b6ce2c3cdc840680efa8b157b27
つき 滝猫 <systakineko.tfs@gmail.com>
2023-08-16 20:12:15 +0700

    Create uinput.hpp

---
+++ jupyter-cpp-kernel/resources/input/uinput.hpp
@@ -0,0 +1,22 @@
+/*==============================================================================
+User input method for Jupyter C++ kernel
+
+Copyright (C) 2023 Tsuki Takineko
+
+Component: Component Header
+
+Purpose:   Provides user input method for Jupyter C++ kernel
+Owner:     Tsuki Takineko (signed off by Tsuki Takineko)
+Platform:  Windows (WIN32), macOS (X and later), Unix/Linux
+First created by: Tsuki Takineko (signed-off by Tsuki Takineko)
+
+Help Link: https://github.com/takinekotfs/jupyter-cpp-kernel
+Report issue: https://github.com/takinekotfs/jupyter-cpp-kernel/issues
+==============================================================================*/
+
+#ifndef UINPUT_HPP
+#define UINPUT_HPP
+
+#define GET_INPUT_STREAM_JP "<inputRequest>"
+
+#endif
