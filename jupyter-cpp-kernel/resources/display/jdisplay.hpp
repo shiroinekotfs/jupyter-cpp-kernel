a71c0f442790582fbd86ce3b5ccfe3b023dbe463
つき 滝猫 <systakineko.tfs@gmail.com>
2023-08-16 19:47:16 +0700

    Create jdisplay.hpp

---
+++ jupyter-cpp-kernel/resources/display/jdisplay.hpp
@@ -0,0 +1,36 @@
+/*==============================================================================
+Display Header for C++ kernel for Jupyter
+
+Copyright (C) 2023 Tsuki Takineko
+
+Component: Component Header
+
+Purpose:   Display graphical user interfaces, including images, video, bars,
+           and document languages (markdown, html, latex) for Jupyter C++ kernel 
+Owner:     Tsuki Takineko
+Platform:  Windows (WIN32), macOS (X and later), Unix/Linux
+First created by: Tsuki Takineko (signed off by Tsuki Takineko)
+==============================================================================*/
+
+#ifndef JDISPLAY_HPP
+#define JDISPLAY_HPP
+
+#include <iostream>
+#include <random>
+
+#include "../basicf/json.hpp"
+#include "encrypt.hpp"
+
+extern "C" {
+#ifdef _WIN32
+    #include "../basicf/dlfcn.h"
+#else
+    #include <dlfcn.h>
+#endif
+}
+
+namespace display {
+    
+}
+
+#endif
