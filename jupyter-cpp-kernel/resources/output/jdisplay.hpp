a05e046ca0d0e39e72f5871fe0f2688b33910304
つき 滝猫 <systakineko.tfs@gmail.com>
2023-08-20 09:51:29 +0700

    Update jdisplay.hpp

--- jupyter-cpp-kernel/resources/output/jdisplay.hpp
+++ jupyter-cpp-kernel/resources/output/jdisplay.hpp
@@ -33,15 +33,15 @@ namespace jdisplay {
         std::cout << "```\n" << code << "\n```" << std::endl;
     }
 
-    void Image (std::string data) {
-        std::cout << "<img src=\"" << data << "\" />" << std::endl;
+    void Image (std::string data, std::string width, std::string height) {
+        std::cout << "<img src=\"" << data << "\" width=\"" << width<< "\" height=\"" << height << "\" />" << std::endl;
     }
 
     void PDF (std::string pdffile, std::string width, std::string height) {
         std::cout << "<object data=\"" << pdffile << "\" type=\"application/pdf\" width=\"" << width << "\" height=\"" << height << "\" ><embed src=\"" << pdffile << "\"></embed></object>" << std::endl;
     }
 
-    void Video (std::string video) {
-        std::cout << "<video controls src=\"" << video << "\">animation</video>" << std::endl;
+    void Video (std::string video, std::string width, std::string height) {
+        std::cout << "<video width=\"" << width << "\" height=\"" << height << "\" controls src=\"" << video << "\">animation</video>" << std::endl;
     }
 }
