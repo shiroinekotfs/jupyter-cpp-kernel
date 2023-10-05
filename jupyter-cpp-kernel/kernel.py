5ef05736e0b9aacaac263ebbdb1e067ff082cfad
takinekotfs <systakineko.tfs@gmail.com>
2023-10-05 14:28:50 +0700

    Revert "Update kernel.py"
    This reverts commit 0e16560575a4b67047d6787d5b78891afe23329e.

--- jupyter-cpp-kernel/kernel.py
+++ jupyter-cpp-kernel/kernel.py
@@ -123,7 +123,7 @@ class CPPKernel(Kernel):
         self.master_path = mastertemp[1]    
         self.resDir = path.join(path.dirname(path.realpath(__file__)), 'resources')
         filepath = path.join(self.resDir, 'master.cpp')
-        subprocess.call(['g++', filepath, '-std=c++14', '-Wno-unused-but-set-variable', '-Wno-unused-parameter', '-Wno-unused-variable', '-w', '-o', self.master_path])
+        subprocess.call(['g++', filepath, '-std=c++14', '-Wno-unused-but-set-variable', '-Wno-unused-parameter', '-Wno-unused-variable', '-ldl', '-w', '-o', self.master_path])
 
     def cleanup_files(self):
         """Remove all the temporary files created by the kernel"""
