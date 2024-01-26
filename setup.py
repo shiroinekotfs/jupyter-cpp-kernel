4a9469434c20cbe5afa8519a5246de34fdcadeda
ねこ 白い <shiroineko.tfs@gmail.com>
2024-01-26 08:05:29 +0700

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='jupyter-cpp-kernel',
-      version='1.0.0a5',
+      version='1.0.0a6',
       description='C++ 14 kernel for Jupyter',
       author='shiroinekotfs',
       author_email='shiroineko.tfs@gmail.com',
