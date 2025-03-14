5478536ab4d782012d31538f5c08f39ed9688c4a
shiroinekotfs <shiroineko.tfs@gmail.com>
2025-03-14 20:49:52 +0700

    update c++23
    Co-Authored-By: Vo Luu Tuong Anh <176574466+anhvlt-2k6@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -25,13 +25,24 @@ setup(
                 'jupyter-cpp-kernel-14',
                 'jupyter-cpp-kernel-17',
                 'jupyter-cpp-kernel-20',
-                ],
+                'jupyter-cpp-kernel-23'],
       keywords=['windows', 'macos', 'linux', 'jupyter', 'cpp', 'jupyter-kernels', 'pip'],
       include_package_data=True,
       data_files=[
-          ("share/jupyter/kernels/cpp98", ["jupyter-cpp-kernel-98/kernel_spec/logo-64x64.png", "jupyter-cpp-kernel-98/kernel_spec/kernel.json", "jupyter-cpp-kernel-98/kernel_spec/logo-32x32.png", "jupyter-cpp-kernel-98/kernel_spec/logo-svg.svg"]),
-          ("share/jupyter/kernels/cpp14", ["jupyter-cpp-kernel-14/kernel_spec/logo-64x64.png", "jupyter-cpp-kernel-14/kernel_spec/kernel.json", "jupyter-cpp-kernel-14/kernel_spec/logo-32x32.png", "jupyter-cpp-kernel-14/kernel_spec/logo-svg.svg"]),
-          
+          ("share/jupyter/kernels/cpp98", 
+            ["jupyter-cpp-kernel-98/kernel_spec/logo-64x64.png", "jupyter-cpp-kernel-98/kernel_spec/kernel.json", "jupyter-cpp-kernel-98/kernel_spec/logo-32x32.png", "jupyter-cpp-kernel-98/kernel_spec/logo-svg.svg"]),
+          ("share/jupyter/kernels/cpp03", 
+            ["jupyter-cpp-kernel-03/kernel_spec/logo-64x64.png", "jupyter-cpp-kernel-03/kernel_spec/kernel.json", "jupyter-cpp-kernel-03/kernel_spec/logo-32x32.png", "jupyter-cpp-kernel-03/kernel_spec/logo-svg.svg"]),
+          ("share/jupyter/kernels/cpp11", 
+            ["jupyter-cpp-kernel-11/kernel_spec/logo-64x64.png", "jupyter-cpp-kernel-11/kernel_spec/kernel.json", "jupyter-cpp-kernel-11/kernel_spec/logo-32x32.png", "jupyter-cpp-kernel-11/kernel_spec/logo-svg.svg"]),
+          ("share/jupyter/kernels/cpp14", 
+            ["jupyter-cpp-kernel-14/kernel_spec/logo-64x64.png", "jupyter-cpp-kernel-14/kernel_spec/kernel.json", "jupyter-cpp-kernel-14/kernel_spec/logo-32x32.png", "jupyter-cpp-kernel-14/kernel_spec/logo-svg.svg"]),
+          ("share/jupyter/kernels/cpp17", 
+            ["jupyter-cpp-kernel-17/kernel_spec/logo-64x64.png", "jupyter-cpp-kernel-17/kernel_spec/kernel.json", "jupyter-cpp-kernel-17/kernel_spec/logo-32x32.png", "jupyter-cpp-kernel-17/kernel_spec/logo-svg.svg"]),
+          ("share/jupyter/kernels/cpp20", 
+            ["jupyter-cpp-kernel-20/kernel_spec/logo-64x64.png", "jupyter-cpp-kernel-20/kernel_spec/kernel.json", "jupyter-cpp-kernel-20/kernel_spec/logo-32x32.png", "jupyter-cpp-kernel-20/kernel_spec/logo-svg.svg"]),
+          ("share/jupyter/kernels/cpp23", 
+            ["jupyter-cpp-kernel-23/kernel_spec/logo-64x64.png", "jupyter-cpp-kernel-23/kernel_spec/kernel.json", "jupyter-cpp-kernel-23/kernel_spec/logo-32x32.png", "jupyter-cpp-kernel-23/kernel_spec/logo-svg.svg"]),
           ("share/cpp_header", ["share/cpp_header/check_cpp.hpp"])
       ]
 )
