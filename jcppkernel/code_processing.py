6f0d5f64c7de70bb705e13ee6d438d0a310750fd
shiroinekotfs <shiroineko.tfs@gmail.com>
2025-04-11 14:45:09 +0700

    fix for #41
    Co-Authored-By: Vo Luu Tuong Anh <176574466+anhvlt-2k6@users.noreply.github.com>

--- jcppkernel/code_processing.py
+++ jcppkernel/code_processing.py
@@ -3,20 +3,15 @@ from re import sub as code_sub, search as code_search, compile as code_compile
 from .external_header import CPPExternalHeader
 
 class CPPCodeProcessingUnit:
-    
     main_head = 'int main() {'
     main_foot = '\treturn 0;\n}'
     comments = r'\/\/.*?(?=\r?\n)|\/\*[\s\S]*?\*\/'
     main_method = r'\bmain\s*\(|^\s*#\s*define\s+\w+\s+main\b'
-    
-    def _prep_main_function(self, code):
-        code = f"{self.main_head}\n{code}\n{self.main_foot}"
-        return code
 
     def _add_code_compat(self, code, cpp_res_path):
         code = code_sub(self.comments, r'', code)
         if not code_search(self.main_method, code):
-            code = self._prep_main_function(code)
+            code = f"{self.main_head}\n{code}\n{self.main_foot}"
         
         code = f"#include {cpp_res_path}\n{code}"
         code = CPPExternalHeader._support_external_header(CPPExternalHeader, code)
--- jcppkernel/temp_file_processing.py
+++ jcppkernel/temp_file_processing.py
@@ -2,12 +2,10 @@ from os import remove, path
 from tempfile import NamedTemporaryFile
 
 class CPPTempFileProcessing:
-    
     def _new_temp_file(self, files, **kwargs):
         file = NamedTemporaryFile(delete=False, mode="w", **kwargs)
         files.append(file.name)
         return file
-
     
     def _cleanup_files(self, master_path, files):
         for file in files:
