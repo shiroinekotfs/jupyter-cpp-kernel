9738b5ea2b0cbd0b36d6dc5993c97d8b88c38ca4
shiroinekotfs <shiroineko.tfs@gmail.com>
2025-04-11 14:45:09 +0700

    fix for #41
    Co-Authored-By: Vo Luu Tuong Anh <176574466+anhvlt-2k6@users.noreply.github.com>

--- jcppkernel/external_header.py
+++ jcppkernel/external_header.py
@@ -3,23 +3,19 @@ from os import path, scandir
 
 class CPPExternalHeader:
     def _find_local_header(self):
-        if hasattr(self, "_cached_local_header"):
-            return self._cached_local_header
         search_paths = [path.abspath(path.dirname(__file__)), prefix]
         for base in search_paths:
             current = base
             while current != path.sep:
                 cpp_header_path = path.join(current, "share", "cpp_header")
                 if path.exists(path.join(cpp_header_path, "check_cpp.hpp")):
-                    self._cached_local_header = cpp_header_path
                     return cpp_header_path
                 new_current = path.dirname(current)
                 if new_current == current:
                     break
                 current = new_current
-        self._cached_local_header = ""
         return ""
-
+    
     def _support_external_header(self, code):
         DATA_FILES_PATH = self._find_local_header(self)
         includes = []
